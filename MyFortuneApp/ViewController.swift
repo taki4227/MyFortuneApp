000f4940375a5ba6b44bed4cea612c8927635821
Naoki Takimoto <takiki.truth@gmail.com>
2017-02-26 18:40:17 +0900

    おみくじアプリの作成

--- MyFortuneApp/ViewController.swift
+++ MyFortuneApp/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MyFortuneLabel: UILabel!
+    
+    @IBAction func getFortune(_ sender: Any) {
+        
+        let results = ["大吉", "吉", "中吉", "凶", "大凶"]
+        
+        // 0~9までの乱数を生成
+        let randomNum = arc4random_uniform(UInt32(results.count))
+        
+        self.MyFortuneLabel.text = results[Int(randomNum)]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 枠線の設定
+//        MyFortuneLabel.layer.borderColor = UIColor.orange.cgColor
+//        MyFortuneLabel.layer.borderWidth = 5
+        
+        // 角丸の設定
+        // 円にするには、Labelの高さの1/2にする
+        MyFortuneLabel.layer.cornerRadius = MyFortuneLabel.bounds.width / 2
+        // 角丸に合わせてマスクする
+        MyFortuneLabel.layer.masksToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
